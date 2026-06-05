c06a3cb14f0a1266774cd859198bea432ab9d2d4
parmarbhavdip <bhavdipsinhparmar245@gmail.com>
2026-06-05 19:42:16 +0530

    Delete 1_demo.js

--- 1_demo.js
+++
@@ -1,7 +0,0 @@
-function printDate()
-{
-    let current = new Date();
-    let today = current.getDate() + "/" + (current.getMonth()+ 1) + "/" + current.getFullYear();
-    console.log(today);
-}
-printDate();
